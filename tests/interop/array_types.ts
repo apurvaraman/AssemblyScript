fa82cc39de312c66b98bd32b5452a66157ac03a6
Sanat Sharma <sanatcolumban@gmail.com>
2017-06-28 14:27:00 -0700

    added array types test

---
+++ tests/interop/array_types.ts
@@ -0,0 +1,44 @@
+//! { "memoryModel": "exportmalloc" }
+
+let a: int[] = new Array<int>(3);
+let b: String[] = new Array<String>(3); 
+let c: float[] = new Array<float>(3);
+let d: double[] = new Array(3); //does it automatically recognize
+
+export function initIntArr(){
+    a[0] = 1.0;
+    a[1] = 2;
+}
+
+export function getIntArrayElem(val: uint): int{
+    return a[val];
+}
+export function initStringArr(){
+    b[0] = "hi";
+    b[1] = "we are messing";
+    b[2] = "around with types";
+}
+
+export function getStringArrElem(val: uint): String{
+    return b[val];
+}
+
+export function initFloatArr(){
+    c[0] = 12.5;
+    c[1] = 23.2;
+    c[2] = 1234;
+}
+
+export function getFloatArrElem(val: uint): float{
+    return c[val];
+}
+
+export function initDoubleArr(){
+    d[0] = 1212.5;
+    d[1] = 2311.2;
+    d[2] = 123.34;
+}
+
+export function getDoubleArrElem(val: uint): double{
+    return d[val];
+}
