a20f08a52812c045852dacad03c2ebc3a2f3422d
Sanat Sharma <sanatcolumban@gmail.com>
2017-07-21 11:00:43 -0700

    Changed string test back to normal

--- tests/interop/string.ts
+++ tests/interop/string.ts
@@ -2,33 +2,14 @@
 
 let a: string = "abc";
 
-export function getString(): int {
-  let v: int[] = new Array(10);
-  v[0] = 1;
-  v[1] = 17;
-  v[2] = 15;
-  v[3] = 71;
-  v[4] = 17;
-  v[5] = 16;
-  v[6] = 14;
-  v[7] = 32;
-  v[8] = 12;
-  
-  return v.length;
-//  return a;
+export function getString(): String {
+  return a;
 }
 
-export function getElem(c: int): int {
-  let v: int[] = new Array(10);
-  for(let i: int = 0;i<10;i++){
-    v[i] = i;
-  }
-  return v[c];
-}
 export function setString(b: string): void {
   a = b;
 }
 
-// export function length() {
-//   let b = a.getLength();
-// }
+export function length(): int {
+  return a.length;
+}
